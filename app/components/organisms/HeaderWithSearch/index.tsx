e6f32f78c494dddaedb5a1bdcceec95bf149d3c9
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2025-11-20 09:20:08 +0000

    logo

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -3,7 +3,7 @@ import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
 import { useOptionalUser } from '#app/utils/user.ts'
-import logo from '~/assets/svg/icon-placeholder.svg'
+import logo from '~/assets/png/logo.png'
 
 export default function HeaderWithSearch() {
 	const matches = useMatches()
