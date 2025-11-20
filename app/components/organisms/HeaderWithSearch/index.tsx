06c16a9fbd7f132afe2752aec621acac6fbd995d
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2025-11-20 10:24:13 +0000

    removed links, need to figure out styling

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -13,32 +13,12 @@ export default function HeaderWithSearch() {
 
 	return (
 		<header className="m-1 py-0.5">
-			<nav className="flex flex-wrap items-center justify-between border-b-1 p-10 sm:flex-nowrap md:gap-8">
-				<div className="flex items-center gap-10">
-					{user ? (
-						<UserDropdown />
-					) : (
-						<Button asChild variant="default" size="lg">
-							<Link to="/">nav link</Link>
-						</Button>
-					)}
-					{user ? (
-						<UserDropdown />
-					) : (
-						<Button asChild variant="default" size="lg">
-							<Link to="/">nav link</Link>
-						</Button>
-					)}
-					{user ? (
-						<UserDropdown />
-					) : (
-						<Button asChild variant="default" size="lg">
-							<Link to="/">nav link</Link>
-						</Button>
-					)}
-				</div>
-				<div>
-					<img className="w-20" src={logo} alt="" />
+			<nav className="flex flex-wrap items-center justify-evenly border-b-1 p-10 sm:flex-nowrap md:gap-8">
+				<div className="flex items-center gap-4">
+					<Link to="/" className="flex w-20 items-center justify-center">
+						<img src={logo} alt="Epic News Logo" />
+					</Link>
+					<span className="text-foreground text-sm">Epic News</span>
 				</div>
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
