9dc76b7e3b46307bee43d94fe02a857079087304
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2025-11-21 22:27:43 +0000

    changed card colour to react with color switch

--- app/components/organisms/Cards/card.tsx
+++ app/components/organisms/Cards/card.tsx
@@ -8,14 +8,14 @@ interface CardProps {
 
 export default function Card({ img, name, role }: CardProps) {
 	return (
-		<div className="m-2 flex flex-col items-center rounded-xl bg-gray-800 p-10 px-17">
+		<div className="m-2 flex flex-col items-center rounded-xl p-10 px-17">
 			<img
 				className="mb-5 w-50 rounded-full shadow-2xl"
 				src={img}
 				alt="portrait"
 			/>
-			<h1 className="text-body-sm mt-1 mb-1 font-bold text-white">{name}</h1>
-			<p className="font-light text-gray-400">{role}</p>
+			<h1 className="text-body-sm mt-1 mb-1 font-bold">{name}</h1>
+			<p className="font-light">{role}</p>
 			<div className="mt-7.5 flex items-center">
 				<ShareButtons />
 			</div>
--- app/root.tsx
+++ app/root.tsx
@@ -48,7 +48,7 @@ export default function App() {
 							</HeroCallToAction>
 						</div>
 						<h1 className="text-mega m-10">Meet the team!</h1>
-						<div className="m-4 flex items-center justify-between gap-4 sm:flex-col md:flex-row">
+						<div className="m-4 flex items-center justify-between gap-4 sm:flex-col lg:flex-row">
 							<Card
 								img={portrait1}
 								name="Leonard Krasner"
