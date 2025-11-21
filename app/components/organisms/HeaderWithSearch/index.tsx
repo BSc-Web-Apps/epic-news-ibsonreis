78cd63c84f07423f7bd47b1d990f6f415a3ecfcb
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2025-11-21 22:14:39 +0000

    updated header styling
    - New logos (colour scheme undecided)
    - Added nav links
    - Altered responsive design

--- app/components/organisms/HeaderWithSearch/index.tsx
+++ app/components/organisms/HeaderWithSearch/index.tsx
@@ -1,9 +1,9 @@
-import { useMatches, Link } from 'react-router'
+import { useMatches, Link, NavLink } from 'react-router'
 import { SearchBar } from '#app/components/search-bar.tsx'
 import { Button } from '#app/components/ui/button.tsx'
 import { UserDropdown } from '#app/components/user-dropdown.tsx'
 import { useOptionalUser } from '#app/utils/user.ts'
-import logo from '~/assets/png/epic-news-logo.png'
+import logo from '~/assets/png/epic-news-logo-brown-dark.png'
 
 export default function HeaderWithSearch() {
 	const matches = useMatches()
@@ -13,33 +13,60 @@ export default function HeaderWithSearch() {
 
 	return (
 		<header className="m-1 py-0.5">
-			<nav className="flex flex-wrap items-center justify-evenly border-b-1 p-10 sm:flex-nowrap md:gap-8">
-				{/* parent 'ish' div */}
+			<nav className="flex items-center justify-between border-b-1 p-10 sm:flex-nowrap md:gap-8">
+				{/* nav link divs */}
+				<div className="text-secondary-foreground dark:text-dark-secondary-foreground mr-auto flex flex-col items-start gap-10 py-8 font-bold lg:flex-row">
+					<div className="lg:p-3">
+						<NavLink to="#">Nav Label</NavLink>
+					</div>
+					<div className="lg:p-3">
+						<NavLink to="#">Nav Label</NavLink>
+					</div>
+					<div className="lg:p-3">
+						<NavLink to="#">Nav Label</NavLink>
+					</div>
+				</div>
+
+				{/* logo */}
 				<div className="flex items-center gap-4">
-					{/* logo */}
-					<Link to="/" className="flex w-20 items-center justify-center">
+					<Link to="/" className="flex w-30 items-center justify-center">
 						<img src={logo} alt="Epic News Logo" />
 					</Link>
-					<span className="text-foreground text-sm">Epic News</span>
 				</div>
 
 				{/* search bar */}
 				<div className="ml-auto hidden max-w-sm flex-1 sm:block">
 					{searchBar}
-				</div>
 
-				{/* button */}
-				<div className="flex items-center gap-10">
-					{user ? (
-						<UserDropdown />
-					) : (
-						<Button asChild variant="default" size="lg">
-							<Link to="/login">Log In</Link>
-						</Button>
-					)}
+					{/* buttons (login / sign up) */}
+					<div className="flex items-center gap-5">
+						{user ? (
+							<UserDropdown />
+						) : (
+							<Button
+								asChild
+								variant="default"
+								size="lg"
+								className="mt-4 w-full"
+							>
+								<Link to="/login">Log In</Link>
+							</Button>
+						)}
+						{user ? (
+							<UserDropdown />
+						) : (
+							<Button
+								asChild
+								variant="default"
+								size="lg"
+								className="mt-4 w-full"
+							>
+								<Link to="/Sign Up">Sign Up</Link>
+							</Button>
+						)}
+					</div>
+					<div className="block w-full sm:hidden">{searchBar}</div>
 				</div>
-
-				<div className="block w-full sm:hidden">{searchBar}</div>
 			</nav>
 		</header>
 	)
