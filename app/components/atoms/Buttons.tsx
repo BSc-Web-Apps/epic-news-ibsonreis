451c6c2bb93ad56b8d209d76bae7b14298c3642a
ibsonreis <145687759+ibsonreis@users.noreply.github.com>
2025-11-25 13:01:37 +0000

    update buttons.tsx colour

--- app/components/atoms/Buttons.tsx
+++ app/components/atoms/Buttons.tsx
@@ -1,6 +1,6 @@
 export default function () {
 	return (
-		<button className="bg-accent cursor-pointer rounded-lg p-3 shadow-md md:p-5">
+		<button className="bg-primary text-accent-foreground cursor-pointer rounded-lg p-3 shadow-md md:p-5">
 			Press Me!
 		</button>
 	)
